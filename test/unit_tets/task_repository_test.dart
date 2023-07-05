bd9ee3d41352d505486f86e58305760c85e45e05
f1xgun <msnzlmm@mail.ru>
2023-07-05 15:54:56 +0400

    fix: task repository tests

--- test/unit_tets/task_repository_test.dart
+++ test/unit_tets/task_repository_test.dart
@@ -1,12 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:todo_list_app/features/tasks/data/repository/tasks_repository.dart';
-import 'package:todo_list_app/features/tasks/domain/models/response_data.dart';
 import 'package:todo_list_app/features/tasks/domain/models/task_model.dart';
 
 import '../data/tasks.dart';
-import '../test_doubles/local_tasks_api_mock.mocks.dart';
-import '../test_doubles/network_tasks_api_mock.mocks.dart';
+// import '../test_doubles/local_tasks_api_mock.mocks.dart';
+// import '../test_doubles/network_tasks_api_mock.mocks.dart';
+import '../test_doubles/local_tasks_api_mock.dart';
+import '../test_doubles/network_tasks_api_mock.dart';
 import '../test_doubles/persistence_manager_mock.mocks.dart';
 
 void main() {
@@ -27,60 +28,69 @@ void main() {
 
   test('Merge when storages have same revision and same tasks', () async {
     final tasks = mockTasks.map(Task.fromJson).toList();
-    when(localStorage.getTasks()).thenAnswer((_) async => tasks);
+    localStorage.setTasks(tasks);
+    networkStorage.setTasks(tasks);
     when(persistenceManager.getTasksRevision()).thenAnswer((_) async => 1);
-    when(networkStorage.getTasks())
-        .thenAnswer((_) async => ResponseData(200, tasks, 1));
-    when(networkStorage.syncTasks(await localStorage.getTasks()))
-        .thenAnswer((_) async => ResponseData(200, tasks, 1));
 
     expect(await repository.syncStorages(), tasks);
-    verify(localStorage.getTasks());
-    verifyNoMoreInteractions(localStorage);
-    verify(networkStorage.getTasks());
-    verifyNoMoreInteractions(networkStorage);
   });
 
-  test('Merge when storages have same revision, but different tasks', () async {
+  test(
+      'Merge when storages have same revision, but different tasks '
+      'Local storage has more new tasks', () async {
     final tasks = mockTasks.map(Task.fromJson).toList();
-    when(localStorage.getTasks()).thenAnswer((_) async => tasks);
-    when(persistenceManager.getTasksRevision()).thenAnswer((_) async => 1);
-    when(networkStorage.getTasks())
-        .thenAnswer((_) async => ResponseData(200, tasks.sublist(2), 1));
-    when(networkStorage.syncTasks(await localStorage.getTasks()))
-        .thenAnswer((_) async => ResponseData(200, tasks, 2));
+    localStorage.setTasks(tasks);
+    networkStorage.setTasks(tasks.sublist(2));
 
-    expect(await repository.getTasks(), tasks);
-    verify(localStorage.getTasks());
-    verifyNoMoreInteractions(localStorage);
-    verify(networkStorage.getTasks());
-    verifyNoMoreInteractions(networkStorage);
+    when(persistenceManager.getTasksRevision()).thenAnswer((_) async => 1);
+
+    expect(await repository.syncStorages(), unorderedEquals(tasks));
   });
 
-  // test('Merge when storages have different revision and different tasks',
-  //     () async {
-  //   final tasks = mockTasks.map(Task.fromJson).toList();
-  //   when(localStorage.getTasks()).thenAnswer((_) async => tasks);
-  //   when(persistenceManager.getTasksRevision()).thenAnswer((_) async => 1);
-  //   when(networkStorage.getTasks())
-  //       .thenAnswer((_) async => ResponseData(200, tasks.sublist(2), 2));
-  //   when(networkStorage.syncTasks(await localStorage.getTasks()))
-  //       .thenAnswer((_) async => ResponseData(200, tasks, 3));
+  test(
+      'Merge when storages have same revision, but different tasks '
+      'Network storage has more tasks', () async {
+    final tasks = mockTasks.map(Task.fromJson).toList();
+    localStorage.setTasks(tasks.sublist(2));
+    networkStorage.setTasks(tasks);
 
-  //   expect(await repository.getTasks(), tasks);
-  // });
+    when(persistenceManager.getTasksRevision()).thenAnswer((_) async => 1);
 
-  // test('Merge when local storage has deleted tasks, which network storage has',
-  //     () async {
-  //   final localTasks = [mockTasks.map(Task.fromJson).first.copyWith(deleted: true)];
-  //   final networkTasks = [mockTasks.map(Task.fromJson).first];
-  //   when(localStorage.getTasks()).thenAnswer((_) async => localTasks);
-  //   when(persistenceManager.getTasksRevision()).thenAnswer((_) async => 1);
-  //   when(networkStorage.getTasks())
-  //       .thenAnswer((_) async => ResponseData(200, networkTasks, 2));
-  //   when(networkStorage.syncTasks())
-  //       .thenAnswer((_) async => ResponseData(200, tasks, 3));
+    expect(await repository.syncStorages(), unorderedEquals(tasks));
+  });
 
-  //   expect(await repository.getTasks(), tasks);
-  // });
+  test(
+      'Merge when storages have different revision and different tasks '
+      'Local storage has more new tasks', () async {
+    final tasks = mockTasks.map(Task.fromJson).toList();
+    localStorage.setTasks(tasks);
+    networkStorage.setTasks(tasks.sublist(3));
+    when(persistenceManager.getTasksRevision()).thenAnswer((_) async => 0);
+
+    expect(await repository.syncStorages(), unorderedEquals(tasks));
+  });
+
+  test(
+      'Merge when storages have different revision and different tasks '
+      'Network storage has more tasks', () async {
+    final tasks = mockTasks.map(Task.fromJson).toList();
+    localStorage.setTasks(tasks.sublist(3));
+    networkStorage.setTasks(tasks);
+    when(persistenceManager.getTasksRevision()).thenAnswer((_) async => 0);
+
+    expect(await repository.syncStorages(), unorderedEquals(tasks));
+  });
+
+  test('Merge when local storage has deleted tasks, which network storage has',
+      () async {
+    final localTasks = [
+      mockTasks.map(Task.fromJson).first.copyWith(deleted: true)
+    ];
+    final networkTasks = [mockTasks.map(Task.fromJson).first];
+    localStorage.setTasks(localTasks);
+    networkStorage.setTasks(networkTasks);
+    when(persistenceManager.getTasksRevision()).thenAnswer((_) async => 1);
+
+    expect(await repository.syncStorages(), []);
+  });
 }
